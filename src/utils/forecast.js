b8e7e16c3ebe64f8a87f8271afe305dd54a26ad9
Raunaq Kapoor <raunaq.kapoor@gmail.com>
2019-07-06 16:58:32 +0530

    Add high and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + Math.round(body.currently.precipProbability*100) + '% chance of rain.'
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out, with a high of ' + body.daily.data[0].temperatureHigh + ' degrees and a low of '+ body.daily.data[0].temperatureLow + ' degrees. There is a ' + Math.round(body.currently.precipProbability*100) + '% chance of rain.'
             )
         }
     })
